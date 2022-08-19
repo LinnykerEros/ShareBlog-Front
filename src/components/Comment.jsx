e0cb5ae320a94e63c1beaa430ffabbac7650ce3b
Eros Linnyker <linnykereros@gmail.com>
2022-08-18 22:14:53 -0300

    fix and added: States, Functions and Props

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,70 +1,66 @@
 import { ThumbsUp, Trash } from "phosphor-react";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import styles from "../styles/Comment.module.css";
 import { Avatar } from "./Avatar";
 import { AuthContext } from "../contexts/AuthContext";
-
-import { getComments } from "../service/commentService";
+import { deleteComment } from "../service/commentService";
 import ptBR from "moment/locale/pt-br";
 import moment from "moment";
-export function Comment({ content, onDeleteComment }) {
-  const { user } = useContext(AuthContext);
+export function Comment({
+  content,
+  id,
+  userName,
+  publishedAt,
+  updatingState,
+  userId,
+}) {
   const [likeCount, setLikeCount] = useState(0);
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const comments = await getComments();
-      setComments(comments);
-    })();
-  }, []);
-
+  const { user } = useContext(AuthContext);
   const handleLikeComment = () => {
     setLikeCount(likeCount + 1);
   };
 
-  const handleCommentDelete = (e) => {
-    e.preventDefault();
-    onDeleteComment(content);
+  const handleCommentDelete = async (e) => {
+    await deleteComment(id);
+    updatingState();
   };
 
   return (
-    <div>
-      {comments.map((comment) => {
-        return (
-          <div className={styles.comment}>
-            <Avatar />
-            <div className={styles.commentBox}>
-              <div className={styles.commentContent}>
-                <header>
-                  <div className={styles.authorAndTime}>
-                    <strong>{user.name}</strong>
-                    <time
-                      title="10 de Agosto às 14:01"
-                      dateTime="2022-08-10 14:00:00"
-                    >
-                      {}
-                    </time>
-                  </div>
-                  <button
-                    onClick={handleCommentDelete}
-                    title="Deletar comentário"
-                  >
-                    <Trash size={24} />
-                  </button>
-                </header>
-                <p>{content}</p>
-              </div>
-              <footer>
-                <button onClick={handleLikeComment}>
-                  <ThumbsUp />
-                  Aplaudir <span>{likeCount}</span>
-                </button>
-              </footer>
+    <div key={id} className={styles.comment}>
+      <Avatar />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>{userName}</strong>
+              <time
+                title={moment(publishedAt).format("LLL")}
+                dateTime={moment(publishedAt).format()}
+              >
+                {moment(publishedAt)
+                  .startOf(publishedAt, {
+                    locale: ptBR,
+                  })
+                  .fromNow()}
+              </time>
             </div>
-          </div>
-        );
-      })}
+            {user?.id == userId ? (
+              <button onClick={handleCommentDelete} title="Deletar comentário">
+                <Trash size={24} />
+              </button>
+            ) : (
+              ""
+            )}
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
     </div>
   );
 }
