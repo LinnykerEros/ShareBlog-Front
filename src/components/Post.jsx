563d9d7837b01c11bd703020622eb9b4588a960e
Eros Linnyker <linnykereros@gmail.com>
2022-08-18 22:18:12 -0300

    fix and added: Functions, States and Props

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,132 +1,109 @@
-import { Avatar } from "./Avatar";
 import styles from "../styles/Post.module.css";
-import { Comment } from "./Comment";
 import moment from "moment";
 import ptBR from "moment/locale/pt-br";
-import { useEffect, useState } from "react";
-import { getPosts } from "../service/postService";
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+import { useContext, useEffect, useState } from "react";
 import { createComment } from "../service/commentService";
-
-export function Post() {
+import { AuthContext } from "../contexts/AuthContext";
+import { Pen } from "phosphor-react";
+export function Post({
+  postId,
+  userId,
+  userName,
+  userProfession,
+  content,
+  comment,
+  pusblishedAt,
+  updatingState,
+}) {
+  const { user } = useContext(AuthContext);
   const [comentario, setComentario] = useState("");
-  const [postId, setPostId] = useState();
-  const [comments, setComments] = useState(["Post muito bacana"]);
+
   const [posts, setPosts] = useState([]);
-  const [newCommentText, setNewCommentText] = useState("");
-
-  useEffect(() => {
-    (async () => {
-      const post = await getPosts();
-      setPosts(post);
-    })();
-  }, []);
-  console.log(posts);
-  const handleCreateNewComment = (e) => {
-    e.preventDefault();
-
-    setComments([...comments, newCommentText]);
-    setNewCommentText("");
-  };
-  const handleNewCommentChange = (e) => {
-    setNewCommentText(e.target.value);
-  };
 
   const handleNewComment = (e) => {
     setComentario(e.target.value);
   };
 
-  const handleSubmit = async (userId, postId) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      console.log(comentario, userId, postId);
-      await createComment(comentario, userId, postId);
+      await createComment(comentario, user?.id, postId);
+      setComentario("");
+      updatingState();
     } catch (err) {
       console.log(err);
     }
   };
 
-  // const handleNewCommentInvalid = (e) => {
-
-  //   console.log(e.target.value);
-  //   if(e.target.value)
-  // };
-
-  const deleteComment = (commentToDelete) => {
-    //react nnunca altera uma informação, ele cria uma nova informação
-    // e salva dentro do estado!
-    const commentsWithoutDeleteOne = comments.filter((comment) => {
-      return comment !== commentToDelete;
-    });
-    setComments(commentsWithoutDeleteOne);
-  };
-
   const isNewCommentEmpty = comentario === "" ? true : false;
 
   return (
-    <div>
-      {posts.map((data) => {
-        return (
-          <article className={styles.post}>
-            <header>
-              <div className={styles.author}>
-                <Avatar hasBorder />
+    <article key={postId} className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar hasBorder />
 
-                <div className={styles.authorInfo}>
-                  <strong>{data.user.name}</strong>
-                  <span>{data.user.profession}</span>
-                </div>
-              </div>
-
-              <time
-                title={moment(data.created_at).format("LLL")}
-                dateTime={moment(data.created_at).format()}
-              >
-                {moment(data.created_at)
-                  .startOf(data.created_at, {
-                    locale: ptBR,
-                  })
-                  .fromNow()}
-              </time>
-            </header>
-            <div className={styles.content}>{data.content}</div>
-
-            <form
-              onSubmit={handleCreateNewComment}
-              className={styles.commentForm}
+          <div className={styles.authorInfo}>
+            <strong>{userName}</strong>
+            <span>{userProfession}</span>
+            <time
+              className={styles.time}
+              title={moment(pusblishedAt).format("LLL")}
+              dateTime={moment(pusblishedAt).format()}
             >
-              <strong>Deixe seu feedback!</strong>
-              <textarea
-                name="comment"
-                placeholder="Deixe um comentário"
-                value={comentario}
-                onChange={handleNewComment}
-                required
-              />
+              {moment(pusblishedAt)
+                .startOf(pusblishedAt, {
+                  locale: ptBR,
+                })
+                .fromNow()}
+            </time>
+          </div>
+        </div>
+        {user?.id == userId ? (
+          <button className={styles.editButton} title="Editar publicação">
+            <Pen size={24} />
+          </button>
+        ) : (
+          ""
+        )}
+      </header>
+      <div className={styles.content}>{content}</div>
 
-              <footer>
-                <button
-                  type="submit"
-                  disabled={isNewCommentEmpty}
-                  onClick={() => handleSubmit(data.user.id, data.id)}
-                >
-                  Publicar
-                </button>
-              </footer>
-            </form>
+      <form onSubmit={handleSubmit} className={styles.commentForm}>
+        <strong>Deixe seu feedback!</strong>
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={comentario}
+          onChange={handleNewComment}
+          required
+        />
 
-            <div className={styles.commentList}>
-              {data.comment.map((data) => {
-                return (
-                  <Comment
-                    key={data.id}
-                    content={data.content}
-                    onDeleteComment={deleteComment}
-                  />
-                );
-              })}
-            </div>
-          </article>
-        );
-      })}
-    </div>
+        <footer>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comment?.map((data) => {
+          return (
+            <Comment
+              key={data.id}
+              postId={postId}
+              id={data.id}
+              userName={data.user.name}
+              userId={data.user.id}
+              publishedAt={data.created_at}
+              content={data.content}
+              updatingState={updatingState}
+            />
+          );
+        })}
+      </div>
+    </article>
   );
 }
