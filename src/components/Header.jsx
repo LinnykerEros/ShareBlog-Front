bb2acfe69ae160f995bd8c7372824c4893d62b60
Eros Linnyker <linnykereros@gmail.com>
2022-08-18 22:16:58 -0300

    added: Function signOut

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,28 @@
 import styles from "../styles/Header.module.css";
 import igniteLogo from "../assets/ignite-logo.svg";
+import Cookies from "js-cookie";
+import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { SignOut } from "phosphor-react";
+import { AuthContext } from "../contexts/AuthContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
+  const { signOutUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  // const signOut = () => {
+  //   Cookies.remove("reactauth.token");
+  //  return navigate("/");
+
+  // };
+
   return (
     <header className={styles.header}>
-      <img src={igniteLogo} alt="LogoIgnite" />
-      {/* <strong>Ignite Feed</strong> */}
+      {/* <img src={igniteLogo} alt="LogoIgnite" /> */}
+      {/* <img src={igniteLogo} alt="LogoIgnite" /> */}
+      <button onClick={signOutUser} className={styles.signOut}>
+        <SignOut size={24} />
+      </button>
     </header>
   );
 }
