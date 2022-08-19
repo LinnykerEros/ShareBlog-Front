4838dbed173f12f2343d32e8f2339b98a4e310f8
Eros Linnyker <linnykereros@gmail.com>
2022-08-19 17:21:22 -0300

    added: States, functions(edituser) and toast

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,6 @@
 import styles from "../styles/SideBar.module.css";
+import Cookies from "js-cookie";
+import parseJwt from "../utils/parseJWT";
 import { PencilLine } from "phosphor-react";
 import {
   Modal,
@@ -15,11 +17,62 @@ import {
 import { Input } from "../components/Form/Input";
 import { Avatar } from "./Avatar";
 import { AuthContext } from "../contexts/AuthContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { getUserById, updateUser } from "../service/userService";
+import { toast } from "react-toastify";
 
-export function SideBar() {
-  const { user } = useContext(AuthContext);
+export function SideBar({ updatingState }) {
+  const { user, fetchUser } = useContext(AuthContext);
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [profession, setProfession] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  // console.log(user);
+  useEffect(() => {
+    const token = Cookies.get("reactauth.token");
+
+    (async () => {
+      if (token) {
+        const id = parseJwt(token).id;
+        console.log(id);
+        const users = await getUserById(id);
+        setName(users.name);
+        setEmail(users.email);
+        setProfession(users.profession);
+      }
+    })();
+  }, []);
+
+  const handleUpdateUser = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (confirmPassword === password) {
+        await updateUser(user.id, name, profession, email, password);
+        toast.success(
+          "Usuário atualizado com sucesso",
+          {
+            autoClose: 2000,
+          },
+          onClose()
+        );
+        fetchUser();
+        updatingState();
+      } else {
+        toast.error("Verifique a senha e tente novamente!", {
+          autoClose: 2000,
+        });
+      }
+    } catch (err) {
+      console.log(err.response.data.message);
+      if (!name || !profession || !email || !password) {
+        toast.error(err.response.data.message, { autoClose: 2000 });
+      }
+    }
+  };
 
   return (
     <aside className={styles.sidebar}>
@@ -44,28 +97,65 @@ export function SideBar() {
           {/* <ModalOverlay /> */}
           <ModalContent bg={"#202024"} alignItems={"center"} gap={2}>
             <ModalHeader> Editar seu perfil</ModalHeader>
-            <Input name={"name"} label={"Nome"} width={"80%"} ml={12} />
             <Input
+              defaultValue={name}
+              name={"name"}
+              label={"Nome"}
+              width={"80%"}
+              ml={12}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <Input
+              defaultValue={profession}
               name={"profession"}
               label={"Profissão"}
               width={"80%"}
               ml={12}
+              onChange={(e) => {
+                setProfession(e.target.value);
+              }}
             />
-            <Input name={"email"} label={"E-mail"} width={"80%"} ml={12} />
-            <Input name={"password"} label={"Senha"} width={"80%"} ml={12} />
             <Input
+              defaultValue={email}
+              name={"email"}
+              label={"E-mail"}
+              width={"80%"}
+              ml={12}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <Input
+              type="password"
+              name={"password"}
+              label={"Senha"}
+              width={"80%"}
+              ml={12}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Input
+              type="password"
               name={"confirmPassword"}
               label={"Confirme Senha"}
               width={"80%"}
               ml={12}
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+              }}
             />
             <ModalCloseButton />
 
             <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={onClose}>
+              <Button colorScheme="pink" mr={3} onClick={onClose}>
                 Close
               </Button>
-              <Button variant="ghost">Secondary Action</Button>
+              <Button onClick={handleUpdateUser} variant="ghost">
+                Aplicar alterações
+              </Button>
             </ModalFooter>
           </ModalContent>
         </Modal>
