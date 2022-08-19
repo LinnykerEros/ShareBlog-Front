c4dce4c8bedbe3074f879fdafa2283a760bd4c91
Eros Linnyker <linnykereros@gmail.com>
2022-08-18 22:18:58 -0300

    added: Function and Modal on ButtonEditProfile

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,26 @@
-import { PencilLine } from "phosphor-react";
-
 import styles from "../styles/SideBar.module.css";
+import { PencilLine } from "phosphor-react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 import { Avatar } from "./Avatar";
 import { AuthContext } from "../contexts/AuthContext";
 import { useContext } from "react";
+
 export function SideBar() {
   const { user } = useContext(AuthContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <aside className={styles.sidebar}>
       <img
@@ -20,11 +35,40 @@ export function SideBar() {
         <span>{user?.profession}</span>
       </div>
       <footer className={styles.footer}>
-        <a href="#">
+        <Button onClick={onOpen} className={styles.buttonChakra}>
           {" "}
           <PencilLine size={20} />
           Editar seu perfil
-        </a>
+        </Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          {/* <ModalOverlay /> */}
+          <ModalContent bg={"#202024"} alignItems={"center"} gap={2}>
+            <ModalHeader> Editar seu perfil</ModalHeader>
+            <Input name={"name"} label={"Nome"} width={"80%"} ml={12} />
+            <Input
+              name={"profession"}
+              label={"Profissão"}
+              width={"80%"}
+              ml={12}
+            />
+            <Input name={"email"} label={"E-mail"} width={"80%"} ml={12} />
+            <Input name={"password"} label={"Senha"} width={"80%"} ml={12} />
+            <Input
+              name={"confirmPassword"}
+              label={"Confirme Senha"}
+              width={"80%"}
+              ml={12}
+            />
+            <ModalCloseButton />
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant="ghost">Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </footer>
     </aside>
   );
