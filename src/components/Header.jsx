060b20a57d39e5f26246ce67dcde8fda17b482e1
Eros Linnyker <linnykereros@gmail.com>
2022-08-22 22:26:45 -0300

    AdjusmentsAndAdded: Menu and function(AuthContext)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,25 @@
 import styles from "../styles/Header.module.css";
 import igniteLogo from "../assets/ignite-logo.svg";
 import Cookies from "js-cookie";
-import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import { SignOut } from "phosphor-react";
 import { AuthContext } from "../contexts/AuthContext";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function Header() {
-  const { signOutUser } = useContext(AuthContext);
+  const { user, signOutUser } = useContext(AuthContext);
   const navigate = useNavigate();
   // const signOut = () => {
   //   Cookies.remove("reactauth.token");
@@ -20,9 +31,53 @@ export function Header() {
     <header className={styles.header}>
       {/* <img src={igniteLogo} alt="LogoIgnite" /> */}
       {/* <img src={igniteLogo} alt="LogoIgnite" /> */}
-      <button onClick={signOutUser} className={styles.signOut}>
-        <SignOut size={24} />
-      </button>
+      <p>ShareBlog</p>
+      <p onClick={() => navigate("/app")}>Home</p>
+      <div>
+        <Menu bg="white">
+          <MenuButton
+            className={styles.signOut}
+            as={Button}
+            colorScheme="blue"
+            boxShadow="none"
+            rightIcon={<ChevronDownIcon />}
+          >
+            {user?.name}
+          </MenuButton>
+          <MenuList bg="transparent">
+            <MenuItem
+              fontSize={"1.15rem"}
+              _focus={{ bg: "none" }}
+              _hover={{
+                bg: "#3182ce",
+                color: "white",
+                fontWeight: "bold",
+                boxShadow: "none",
+              }}
+              boxShadow="none"
+              onClick={() => navigate("/userPosts")}
+            >
+              Minhas publicações
+            </MenuItem>
+            <MenuItem
+              gap={150}
+              onClick={signOutUser}
+              transition="all 0.2"
+              fontSize={"1.15rem"}
+              _hover={{
+                bg: "#3182ce",
+                color: "white",
+                fontWeight: "bold",
+                boxShadow: "none",
+              }}
+              boxShadow="none"
+              bg="transparent"
+            >
+              Sair {<SignOut size={24} />}
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
     </header>
   );
 }
