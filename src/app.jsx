c47fb004f89fb5b4fe752a4a66a5ec688f7e7c6a
Eros Linnyker <linnykereros@gmail.com>
2022-08-22 22:18:22 -0300

    added: New route(/usePosts)

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home";
 import { SignIn } from "./pages/login";
 import { Register } from "./pages/register";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { UserPosts } from "./pages/userPosts";
 function App() {
   return (
     <Router>
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SignIn />} exact />
           <Route path="/register" element={<Register />} />
+          <Route path="/userPosts" element={<UserPosts />} />
           <Route path="/app" element={<Home />} />
         </Routes>
       </AuthProvider>
