14816dfadd20a4d404f6583d5efd481dd2d6c6ba
Eros Linnyker <linnykereros@gmail.com>
2022-08-25 16:48:24 -0300

    adjusment: Refactoring array and const randomNumbe

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,6 +1,6 @@
 import styles from "../styles/Avatar.module.css";
 
-const images = [
+const UserLoged = [
   "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
   "https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png",
   "https://i.pinimg.com/474x/bd/ee/4c/bdee4c328550aaf21aa9f43fd19e2136.jpg",
@@ -14,20 +14,23 @@ const AnotherUser = [
   " https://preview.redd.it/sgfxdosc4qo81.png?width=338&format=png&auto=webp&s=68081fe5673ff6ac567a531ae01a786ca80695f6",
   " https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png",
   " https://cdn.dribbble.com/users/2338264/screenshots/6859035/attachments/1462872/owl-icon-revised.jpg?compress=1&resize=400x300&vertical=top",
-  "  https://ih0.redbubble.net/image.618410871.2644/flat,1000x1000,075,f.u2.jpg",
+  " https://ih0.redbubble.net/image.618410871.2644/flat,1000x1000,075,f.u2.jpg",
+  " https://ih0.redbubble.net/image.618410871.2644/flat,1000x1000,075,f.u2.jpg",
+  " https://i.imgur.com/fXJosY6.jpg",
   " https://i.imgur.com/fXJosY6.jpg",
 ];
 
-const randomNumber = Math.floor(Math.random() * 6);
-const randomNumberAnotherUser = Math.floor(Math.random() * 6);
+const randomNumberUserLoged = Math.floor(Math.random() * 6);
 
 export function Avatar({ verifyUserId, hasBorder = false }) {
+  const randomNumberAnotherUser = Math.floor(Math.random() * 8);
+
   return (
     <img
       className={hasBorder ? styles.hasBorder : styles.img}
       src={
         verifyUserId
-          ? images[randomNumber]
+          ? UserLoged[randomNumberUserLoged]
           : AnotherUser[randomNumberAnotherUser]
       }
       alt=""
