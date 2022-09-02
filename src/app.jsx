4a61749608ba84e10c3f2eeb99c9b316bbbcbc75
Eros Linnyker <linnykereros@gmail.com>
2022-09-01 21:35:45 -0300

    added: New Routes

--- src/app.jsx
+++ src/app.jsx
@@ -3,19 +3,28 @@ import Home from "./pages/home";
 import { SignIn } from "./pages/login";
 import { Register } from "./pages/register";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
-import { UserPosts } from "./pages/userPosts";
+import { UserLogedPosts } from "./pages/userLogedPosts";
 import Admin from "./pages/admin";
+import { FilterProvider } from "./contexts/FilterContext";
+import SearchUser from "./pages/searchUser";
+import { Header } from "./components/Header";
+import { UserPosts } from "./pages/userPosts";
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<SignIn />} exact />
-          <Route path="/register" element={<Register />} />
-          <Route path="/userPosts" element={<UserPosts />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/app" element={<Home />} />
-        </Routes>
+        <FilterProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<SignIn />} exact />
+            <Route path="/register" element={<Register />} />
+            <Route path="/userLogedPosts" element={<UserLogedPosts />} />
+            <Route path="/userPosts" element={<UserPosts />} />
+            <Route path="/searchUser" element={<SearchUser />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/app" element={<Home />} />
+          </Routes>
+        </FilterProvider>
       </AuthProvider>
     </Router>
   );
